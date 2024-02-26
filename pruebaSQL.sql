96ac0c918faeaf3b45f4d6a49c02356fc9c943c7
Elian <eliansa123@gmail.com>
2024-02-26 10:24:01 -0500

    Prueba Tecnica Sql Realizada Elian Cañas

---
+++ pruebaSQL.sql
@@ -0,0 +1,36 @@
+Ejercicio 1
+
+SELECT emp_no AS ID EMPLEADO, first_name AS NOMBRE, last_name AS APELLIDO, gender AS GENERO, hire_date AS FECHA CONTRATACION
+FROM employees
+WHERE gender = 'F'
+WHERE hire_date >= '2000-01-01'
+
+Ejercicio 2
+
+SELECT e.emp_no AS ID EMPLEADO, e.first_name AS NOMBRE, e.last_name AS APELLIDO, s.salaries AS MAYOR SALARIO
+FROM employees e
+INNER JOIN salaries s ON e.emp_no = s.emp_no
+
+Ejercicio 3
+
+SELECT e.emp_no AS ID EMPLEADO, e.first_name AS NOMBRE, e.last_name AS APELLIDO, AVG('salary') AS PROMEDIO SALARIO
+FROM employees e
+INNER JOIN salaries s ON e.emp_no = s.emp_no
+
+Ejercicio 4
+
+SELECT e.emp_no AS ID EMPLEADO, e.first_name AS NOMBRE, e.last_name AS APELLIDO,  e.gender AS GENERO, e.birth_date AS FECHA CUMPLEAÑOS, d.dept_name AS DEPARTAMENTO,t.title AS CARGO ACTUAL , e.hire_date AS FECHA CONTRATACION
+FROM employees e
+INNER JOIN dept_manager m ON e.emp_no = m.emp_no
+INNER JOIN departaments d ON e.dept_no = d.dept_no
+INNER JOIN titles t ON e.emp_no = t.emp_no
+
+Ejercicio 5
+
+SELECT e.emp_no AS ID EMPLEADO, e.first_name AS NOMBRE, e.last_name AS APELLIDO, e.birth_date AS FECHA CUMPLEAÑOS, e.hire_date AS FECHA CONTRATACION, t.title AS CARGO ACTUAL, d.dept_name AS DEPARTAMENTO, s.salary
+FROM employees e
+INNER JOIN dept_manager m ON e.emp_no = m.emp_no
+INNER JOIN departaments d ON e.dept_no = d.dept_no
+INNER JOIN titles t ON e.emp_no = t.emp_no
+INNER JOIN salaries s ON s.emp_no = e.emp_no
+
